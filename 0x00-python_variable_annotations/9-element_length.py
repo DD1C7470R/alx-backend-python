6750b822e0a43d39665184464381e9bd10d87c77
DD1C7470R <gracevalley101@gmail.com>
2024-01-11 16:38:45 +0000

    Add task 9 fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Type Checking"""
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of int tuple"""
     return [(i, len(i)) for i in lst]
