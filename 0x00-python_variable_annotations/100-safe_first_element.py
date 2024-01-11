b83fc924adb345633941fcf428c6e7cfe40ec0a7
DD1C7470R <gracevalley101@gmail.com>
2024-01-11 16:46:34 +0000

    Add task 100 fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Type Checking"""
-from typing import Union, Any, List
+from typing import Union, Any, Sequence
 
 
-def safe_first_element(lst: List[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Returns a value or none"""
     if lst:
         return lst[0]
