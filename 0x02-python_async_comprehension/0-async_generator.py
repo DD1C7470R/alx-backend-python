07cf81f409cedcfa25d850c5b4cfbd1131b6bbea
DD1C7470R <gracevalley101@gmail.com>
2024-01-16 18:26:06 +0000

    Hot fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Basic Async Syntax"""
 import asyncio
-from index import async_generator
+import random
+from typing import Generator
 
 
-async def async_comprehension():
+async def async_generator() -> Generator[int, None, None]:
     """will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10"""
-    result = [i async for i in async_generator()]
-    return result
+    for i in range(0, 10):
+        i = random.uniform(0, 10)
+        yield i
+        await asyncio.sleep(1)
