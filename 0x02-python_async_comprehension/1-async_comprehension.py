5248b98fd46a1e7a33fb4df3788d435f6bd2ebaa
DD1C7470R <gracevalley101@gmail.com>
2024-01-16 18:42:59 +0000

    Hot fix task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Basic Async Syntax"""
 import asyncio
-from index import async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """will loop 10 times, each time asynchronously wait 1 second,
