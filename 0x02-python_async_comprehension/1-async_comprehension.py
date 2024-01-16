3060e93ec1a0cc63f3335459ef839bc9b618a60a
DD1C7470R <gracevalley101@gmail.com>
2024-01-16 18:37:53 +0000

    Add return type to task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,9 @@
 """Basic Async Syntax"""
 import asyncio
 from index import async_generator
+from typing import List
 
-
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10"""
     result = [i async for i in async_generator()]
